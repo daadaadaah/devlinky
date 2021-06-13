124860b4c45dfef80fcbf53f033c602b8b819565
daadaadaah <daadaadaah@gmail.com>
2021-06-14 00:32:48 +0900

    feat: Add Header with user profile

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import useCurrentUser from './hooks/useCurrentUser';
+
 export default function Header() {
+  const { currentUser } = useCurrentUser();
+
   return (
     <>
       <img src="../assets/images/logo-small.png" alt="devlinky-logo" />
+      {currentUser && <img src={currentUser.githubProfile} alt="user-profile" />}
     </>
   );
 }
