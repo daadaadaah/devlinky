9ef7751877160778cc7d245b38e7ad951d8bf4b4
daadaadaah <daadaadaah@gmail.com>
2021-06-15 20:55:05 +0900

    Update App for applyingreact-router-dom and adding MainPage and (temp) LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
 import Header from './Header';
+
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
     <>
       <Header />
-      <p>devlinky-chorome-extension</p>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/login" component={LoginPage} />
+      </Switch>
     </>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
@@ -13,16 +15,55 @@ jest.mock('react-redux');
 describe('<App />', () => {
   const dispatch = jest.fn();
 
-  beforeEach(() => {
-    useDispatch.mockImplementation(() => dispatch);
+  function renderApp({ path }) {
+    return render(
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>,
+    );
+  }
 
-    useSelector.mockImplementation((selector) => selector({
-      currentUser,
-    }));
+  context('with any path ', () => {
+    it('shows App name', () => {
+      const { getByAltText } = renderApp({ path: '/' });
+
+      const logo = getByAltText('devlinky-logo');
+      expect(logo).toHaveAttribute('src', '../assets/images/logo-small.png');
+    });
   });
 
-  it('shows user profile', () => {
-    const { container } = render(<App />);
-    expect(container).toHaveTextContent('devlinky');
+  context('with path / & with currentUser', () => {
+    beforeEach(() => {
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        currentUser,
+      }));
+    });
+
+    it('shows menus', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('bookmark');
+      expect(container).toHaveTextContent('list');
+    });
+  });
+
+  context('with path / & without currentUser', () => {
+    beforeEach(() => {
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        currentUser: null,
+      }));
+    });
+
+    it('does not show menus & redirect loginPage', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).not.toHaveTextContent('bookmark');
+      expect(container).not.toHaveTextContent('list');
+      expect(container).toHaveTextContent('login');
+    });
   });
 });
