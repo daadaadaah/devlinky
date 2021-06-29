cf389b845d9f35b2587a57a19adcb9ba1aea4885
daadaadaah <daadaadaah@gmail.com>
2021-06-29 22:33:17 +0900

    feat: Add service for `fetchUrlMetaData` with jest-ignore temporarily

---
+++ src/services/api/index.js
@@ -0,0 +1,26 @@
+export const fetchUrlMetaData = async (url) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+  });
+
+  const html = await response.text();
+
+  const el = document.createElement('div');
+  el.innerHTML = html;
+
+  const title = el.querySelector('meta[property="og:title"]')?.getAttribute('content');
+  const thumbnail = el.querySelector('meta[property="og:image"]')?.getAttribute('content');
+
+  const preview = {
+    url,
+    title,
+    thumbnail,
+  };
+
+  return preview;
+};
+
+export const temp = () => {};
