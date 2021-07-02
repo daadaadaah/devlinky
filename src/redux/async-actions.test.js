364f6c63614f6c388c8e2af5ab79dd2334356f9a
daadaadaah <daadaadaah@gmail.com>
2021-07-02 18:33:39 +0900

    feat: Add async-action for `loadCurrentUser`

--- src/redux/async-actions.test.js
+++ src/redux/async-actions.test.js
@@ -3,18 +3,24 @@ import { getDefaultMiddleware } from '@reduxjs/toolkit';
 import configureStore from 'redux-mock-store';
 
 import {
-  loadUrl, setUrl, fetchPreview, setPreview,
+  loadUrl,
+  setUrl,
+  fetchPreview,
+  setPreview,
+  loadCurrentUser,
+  setCurrentUser,
 } from './slice';
 
 import { fetchUrl } from '../services/chrome';
-import { fetchUrlMetaData } from '../services/api';
+import { fetchUrlMetaData, login } from '../services/api';
 
-import { url, preview } from '../../fixtures';
+import { url, preview, currentUser } from '../../fixtures';
 
 const mockStore = configureStore(getDefaultMiddleware());
 
 jest.mock('../services/api');
 jest.mock('../services/chrome');
+jest.mock('../services/storage/localStorage');
 
 describe('actions', () => {
   let store;
@@ -52,6 +58,24 @@ describe('actions', () => {
       const actions = store.getActions();
 
       expect(actions[0]).toEqual(setPreview(preview));
+    });
+  });
+
+  describe('loadCurrentUser', () => {
+    beforeEach(() => {
+      store = mockStore({
+        currentUser: null,
+      });
+
+      login.mockResolvedValue(currentUser);
+    });
+
+    it('runs setCurrentUser', async () => {
+      await store.dispatch(loadCurrentUser());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toStrictEqual(setCurrentUser(currentUser));
     });
   });
 });
