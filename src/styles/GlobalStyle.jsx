e83e7f638d19dcad5092b4f37b2241cdd3383ba9
daadaadaah <daadaadaah@gmail.com>
2021-07-07 00:14:44 +0900

    style: Use GlobalStyle instead of `<style/>` in index.html

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { Global, css } from '@emotion/react';
+
+import style from './designSystem';
+
+const GlobalStyle = () => (
+  <Global
+    styles={css`
+      * {
+        box-sizing: border-box;
+      }
+
+      body {
+        width: 360px;
+        background-color: ${style.common.background};
+        font-family: ${style.common.font.en.family};
+      }
+    `}
+  />
+);
+
+export default GlobalStyle;
