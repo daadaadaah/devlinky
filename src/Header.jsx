931b034a4e6017a5e52642f65a783c9c24ba5d14
daadaadaah <daadaadaah@gmail.com>
2021-07-07 05:19:38 +0900

    style: Style LoginPage temporarily

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled';
+
 import React from 'react';
 
 import useCurrentUser from './hooks/useCurrentUser';
@@ -6,9 +8,29 @@ export default function Header() {
   const { currentUser } = useCurrentUser();
 
   return (
-    <>
-      <img src="../assets/images/logo-small.png" alt="devlinky-logo" />
-      {currentUser && <img src={currentUser.githubProfile} alt="user-profile" />}
-    </>
+    <Wrapper>
+      <Logo src="../assets/images/logo-small.png" alt="devlinky-logo" />
+      {currentUser && <Profile src={currentUser.githubProfile} alt="user-profile" />}
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  position: absolute;
+  margin: 36px 30px 0 30px;
+`;
+
+const Logo = styled.img`
+  width: 140px;
+  height: 25px;
+
+  border-radius: 0px;
+`;
+
+// TODO : 동그랗게 하기
+const Profile = styled.img`
+  height: 36px;
+  width: 36px;
+
+  border-radius: 0px;
+`;
