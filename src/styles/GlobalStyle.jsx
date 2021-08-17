5d0ad631c218ad01072d18fbf7087a2e6bc9cd7c
daadaadaah <daadaadaah@gmail.com>
2021-08-18 07:21:31 +0900

    style(common): Add font-style: normal

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -16,6 +16,7 @@ const GlobalStyle = () => (
         min-height: 600px;
         background-color: ${style.common.background};
         font-family: ${style.common.font.en.family};
+        font-style: normal;
         color: ${style.common.color};
         /* 임시 : 디자인 QA용 */
         ${process.env.NODE_ENV !== 'production' && `box-shadow: 0 0 0 1px${style.colors.white}`};
