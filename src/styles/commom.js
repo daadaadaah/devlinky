fab16ff889c6ec5c060e35c97f85e13687d498bf
daadaadaah <daadaadaah@gmail.com>
2021-08-18 08:08:15 +0900

    Add comment for input-placeholder, input in common.font.size

--- src/styles/commom.js
+++ src/styles/commom.js
@@ -31,8 +31,8 @@ export const font = {
   },
   size: {
     tiny: '10px', // content-subtitle
-    small: '12px', // input, input-title, content-title, page-number
-    regular: '14px', // welcome 문구
+    small: '12px', // input-placeholder, input-title, content-title, page-number
+    regular: '14px', // welcome 문구, input
     medium: '16px', // button
     large: '20px', // tab menu
   },
