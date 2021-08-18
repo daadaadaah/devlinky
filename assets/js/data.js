ea532a13cd72ba1965fe33a5cfec0bf5885a5765
daadaadaah <daadaadaah@gmail.com>
2021-08-18 12:56:34 +0900

    feat(assets): Add tags

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,8 +8,96 @@ export const teches = [
     color: '#757575',
   },
   {
+    name: 'Typescript',
+    color: '#007ACC',
+  },
+  {
+    name: 'Python',
+    color: '#3877AB',
+  },
+  {
+    name: 'Kotlin',
+    color: '#E352CA',
+  },
+  {
+    name: 'Go',
+    color: '#00ACD7',
+  },
+  {
     name: 'NodeJS',
     color: '#56C23F',
+  },
+  {
+    name: 'HTML/CSS',
+    color: '#18B8B9',
+  },
+  {
+    name: 'React',
+    color: '#00D8FF',
+  },
+  {
+    name: 'VueJS',
+    color: '#41B883',
+  },
+  {
+    name: 'Angular',
+    color: '#E23237',
+  },
+  {
+    name: 'Svelte',
+    color: '#FF5B26',
+  },
+  {
+    name: 'Jest',
+    color: '#B72F56',
+  },
+  {
+    name: 'Git/Github',
+    color: '#2A7755',
+  },
+  {
+    name: 'GraphQL',
+    color: '#E535AB',
+  },
+  {
+    name: 'Webpack',
+    color: '#1C78C0',
+  },
+  {
+    name: 'Babel',
+    color: '#FFCB12',
+  },
+  {
+    name: 'Web',
+    color: '#DE325B',
+  },
+  {
+    name: 'Browser',
+    color: '#FBAD39',
+  },
+  {
+    name: 'Fontend',
+    color: '#B132DE',
+  },
+  {
+    name: 'Backend',
+    color: '#264BCF',
+  },
+  {
+    name: 'DSA',
+    color: '#2A7755',
+  },
+  {
+    name: 'Test',
+    color: '#965CF3',
+  },
+  {
+    name: 'Security',
+    color: '#B72F56',
+  },
+  {
+    name: 'RestAPI',
+    color: '#41B883',
   },
 ];
 
